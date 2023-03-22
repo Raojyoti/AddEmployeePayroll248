1ccc7f61215d7fb4ab141222b6a0d133bd4e2cc6
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2023-03-22 22:19:49 +0530

    [Jyoti Rao] On Document Load Set Event Listeners

---
+++ js/EmployeeRegister.js
@@ -0,0 +1,27 @@
+/*AddEmployeePayroll UC-1 => Modify Employee Payroll Class with new Attributes and Getters and Setters */
+window.addEventListener('DOMContentLoaded', (event) => {
+    const name =document.querySelector('#name');
+    const textError =  document.querySelector('.text-error');
+    name.addEventListener('input', function() {
+        if(name.value.length == 0)
+        {
+            textError.textContent = "";
+            return;
+        }
+        try{
+            (new EmployeePayrollData()).name = name.value;
+            textError.textContent= "";
+        }
+        catch(e)
+        {
+            textError.textContent = e;
+        }
+    });
+
+    const salary = document.querySelector('#salary');
+    const output = document.querySelector('.salary-output');
+    output.textContent= salary.value;
+    salary.addEventListener('input', function() {
+    output.textContent = salary.value;
+   });
+});
